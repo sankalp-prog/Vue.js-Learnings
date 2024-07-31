e64d86fdb96faad0fe7911050f4fe0d761c8a687
sankalp-prog <planetsankalp@gmail.com>
2024-08-01 03:43:01 +0530

    Task2: Methods, v-html and accessing properties from data

--- basics-01/app.js
+++ basics-01/app.js
@@ -3,8 +3,22 @@ const app = Vue.createApp({
     return {
       courseGoal: "Finish the course and learn Vue!",
       vueLink: "https://vuejs.org/guide/",
+      option1: "Learn Vue",
+      option2: "Master Vue",
+      option3: `<h2>This is an HTML Input</h2>`
     };
   },
+  methods: {
+    goalChange() {
+        const randomNumber = Math.random()
+        if (randomNumber < 0.5) {
+            return this.option1
+        }
+        else {
+            return this.option2
+        }
+    }
+  }
 });
 
 app.mount("#user-goal");
