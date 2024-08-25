4832241187d1c9a6965b35ea0f67bf94f0849d5a
sankalp-prog <planetsankalp@gmail.com>
2024-08-25 22:57:09 +0530

    Intro to Components

--- components-intro/app.js
+++ components-intro/app.js
@@ -2,27 +2,48 @@ const app = Vue.createApp({
   data() {
     return {
       details: false,
-      friends: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "01234 5678 991",
-          email: "manuel@localhost.com",
-        },
-        {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "09876 543 221",
-          email: "julie@localhost.com",
-        },
-      ],
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
     };
   },
   methods: {
     toggleDetails() {
-        this.details = !this.details;
+      this.details = !this.details;
     },
+  },
+});
 
+app.component("friend-card", {
+  template: `
+        <li>
+          <h2>{{ friend.name }}</h2>
+          <!-- issue: the same method and data is being reused for each friend which means both the cards toggle when you press any one button -->
+          <button @click="toggleDetails">Show Details</button>
+          <ul v-if="details">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+        </li>
+    `,
+  data() {
+    return {
+      details: false,
+      friend: {
+        id: "julie",
+        name: "Julie Jones",
+        phone: "09876 543 221",
+        email: "julie@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.details = !this.details;
+    },
   },
 });
 
