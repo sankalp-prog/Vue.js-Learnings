f078ebf3d3d7fbf47293458ff0d6df1e527cb17e
sankalp-prog <planetsankalp@gmail.com>
2024-09-20 14:34:37 +0530

    Scoped Slots

--- cmp-adv-01/src/App.vue
+++ cmp-adv-01/src/App.vue
@@ -9,6 +9,21 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <!-- <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+        <p>{{ slotProps.thirdProp }}</p>
+      </template>
+    </course-goals> -->
+
+    <!-- SHORTER WAY TO WRITE THE ABOVE GIVEN THAT THERE IS ONLY ONE SLOT -->
+    <course-goals #default="slotProps">
+        <!-- <h2>{{ slotProps }}</h2> -->
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+        <p>{{ slotProps.thirdProp }}</p>
+    </course-goals>
   </div>
 </template>
 
@@ -16,6 +31,7 @@
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   // First way to write local components-
@@ -36,7 +52,8 @@ export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals,
   },
   data() {
     return {
---
+++ cmp-adv-01/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+        <slot :item="goal" another-prop="..." third-prop="Random things"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["Learn Vue.js", "Finish the course"],
+    };
+  },
+};
+</script>
