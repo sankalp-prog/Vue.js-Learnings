e243529d1b8ae75c35dda778720125462e116256
sankalp-prog <planetsankalp@gmail.com>
2024-09-23 00:51:37 +0530

    Initial Commit

---
+++ cmp-adv-02(Dynamic Components)/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <active-goals></active-goals>
+    <manage-goals></manage-goals>
+  </div>
+</template>
+
+<script>
+import ActiveGoals from "./components/ActiveGoals.vue"
+import ManageGoals from "./components/ManageGoals.vue"
+
+export default {
+  components: {
+    ActiveGoals,
+    ManageGoals,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
