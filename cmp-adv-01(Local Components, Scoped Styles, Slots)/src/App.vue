a73c323868bc2ed656539ced1a46a984dcd697bc
sankalp-prog <planetsankalp@gmail.com>
2024-09-22 23:10:53 +0530

    Renaming the folder

---
+++ cmp-adv-01(Local Components, Scoped Styles, Slots)/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <!-- ANOTHER WAY TO WRITE THE ABOVE TAG: -->
+    <!-- <TheHeader />  -->
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <!-- <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+        <p>{{ slotProps.thirdProp }}</p>
+      </template>
+    </course-goals> -->
+
+    <!-- SHORTER WAY TO WRITE THE ABOVE GIVEN THAT THERE IS ONLY ONE SLOT -->
+    <course-goals #default="slotProps">
+        <!-- <h2>{{ slotProps }}</h2> -->
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+        <p>{{ slotProps.thirdProp }}</p>
+    </course-goals>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
+
+export default {
+  // First way to write local components-
+  // components: {
+  //   'the-header': TheHeader,
+  //   'badge-list': BadgeList,
+  //   'user-info': UserInfo
+  // },
+
+  // Second way-
+  // components: {
+  //   TheHeader: TheHeader,
+  //   BadgeList: BadgeList,
+  //   UserInfo: UserInfo
+  // },
+
+  // Modern way-[Just a shorter way to write the above 2nd method]
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ cmp-adv-01(Local Components, Scoped Styles, Slots)/src/components/UserInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <base-card>
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <!-- if there is no named slot is given then all other content will be put inside the default slot even if the below template tag is not written -->
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["fullName", "infoText", "role"],
+};
+</script>
+
