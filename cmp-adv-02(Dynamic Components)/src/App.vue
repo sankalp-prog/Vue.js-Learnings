eda74326516cb85185d96fcbd3b1b8cdda0d07b6
sankalp-prog <planetsankalp@gmail.com>
2024-10-04 00:41:05 +0530

    Keeping dynamic components alive using keep-alive[cache]

--- cmp-adv-02(Dynamic Components)/src/App.vue
+++ cmp-adv-02(Dynamic Components)/src/App.vue
@@ -5,7 +5,12 @@
     <!-- <active-goals v-if="activeComponent == 'activeGoals'"></active-goals>
     <manage-goals v-if="activeComponent == 'manageGoals'"></manage-goals> -->
     <!-- USING DYNAMIC COMPONENT TO DO THE ABOVE COMMENTED LINES: -->
-    <component :is="activeComponent"></component>
+
+    <!-- PROBLEM: If the user types something in the input field and then switches to another component and then comes back.... the input field is now empty and whatever he wrote previously is not saved -->
+    <!-- keep-alive is used to cache the state of the component, this way if user types in something in the input field, it will remain even if he goes to another component and comes back-->
+    <keep-alive>
+      <component :is="activeComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
--- cmp-adv-02(Dynamic Components)/src/components/ManageGoals.vue
+++ cmp-adv-02(Dynamic Components)/src/components/ManageGoals.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
-        <h2>Manage Goals</h2>
-    </div>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text">
+  </div>
 </template>
