c4e2598744ed0a05c27330687b7dbeb3d2b533e5
sankalp-prog <planetsankalp@gmail.com>
2024-10-04 02:10:23 +0530

    Teleport: Enables semantically correct HTML structure while keeping its functionality intact

--- cmp-adv-02(Dynamic Components)/src/components/ManageGoals.vue
+++ cmp-adv-02(Dynamic Components)/src/components/ManageGoals.vue
@@ -3,11 +3,15 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="input" />
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="isInvalid">
-      <h2>Input is invalid!</h2>
-      <p>Please enter a few characters...</p>
-      <button @click="setDefault">Okay</button>
-    </error-alert>
+    <!-- Using <teleport></teleport>, the error alert component is transport to the body tags when rendered in the browser rather than being nested inside some divs[exactly how we wrote it] -->
+    <!-- This allows the same scope and functionality as intended, just when rendedred you can transport it to where it's sementically correct. -->
+    <teleport to="body">
+      <error-alert v-if="isInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter a few characters...</p>
+        <button @click="setDefault">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
