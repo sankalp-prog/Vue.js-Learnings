81d38f9d6e35d615b4d445ee56e58c26c17cc2a4
sankalp-prog <planetsankalp@gmail.com>
2024-10-04 01:53:23 +0530

    Creating Error Alert component using previous knowledge

--- cmp-adv-02(Dynamic Components)/src/components/ManageGoals.vue
+++ cmp-adv-02(Dynamic Components)/src/components/ManageGoals.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text">
+    <input type="text" ref="input" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="isInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter a few characters...</p>
+      <button @click="setDefault">Okay</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  data() {
+    return {
+      isInvalid: false,
+    };
+  },
+  components: { ErrorAlert },
+  methods: {
+    setGoal() {
+      const goal = this.$refs.input.value;
+      if (goal === "") {
+        this.isInvalid = true;
+      }
+    },
+    setDefault() {
+      this.isInvalid = false;
+    },
+  },
+};
+</script>
